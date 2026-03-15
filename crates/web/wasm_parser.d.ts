e4b70e283907714f5ecca52eaec0e32c92c53c52
Marc Openclaw <openclaw.marc@gmx.de>
2026-03-15 14:08:25 +0100

    Update web UI: sync crates/web/index.html with production features
    - Updated crates/web/index.html to match root index.html
    - Added WASM support (no API server needed)
    - Added console logging and verbose mode
    - Added collapsible tree view
    - Copied WASM build files to crates/web/
    - Fixed import paths for standalone operation

---
+++ crates/web/wasm_parser.d.ts
@@ -0,0 +1,120 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * WASM module wrapper for JavaScript interop
+ */
+export class WasmModule {
+    free(): void;
+    [Symbol.dispose](): void;
+    /**
+     * Encode module to bytes as a Uint8Array
+     */
+    encode(): Uint8Array;
+    /**
+     * Create a new empty module
+     */
+    constructor();
+    /**
+     * Parse WASM binary from a JavaScript Uint8Array
+     */
+    static parse(data: Uint8Array): WasmModule;
+    /**
+     * Get module as JSON representation
+     */
+    toJSON(): string;
+    /**
+     * Get data segment count
+     */
+    readonly dataCount: number;
+    /**
+     * Get element segment count
+     */
+    readonly elementCount: number;
+    /**
+     * Get export count
+     */
+    readonly exportCount: number;
+    /**
+     * Get function count
+     */
+    readonly functionCount: number;
+    /**
+     * Get global count
+     */
+    readonly globalCount: number;
+    /**
+     * Get import count
+     */
+    readonly importCount: number;
+    /**
+     * Get memory count
+     */
+    readonly memoryCount: number;
+    /**
+     * Get table count
+     */
+    readonly tableCount: number;
+    /**
+     * Get type count
+     */
+    readonly typeCount: number;
+}
+
+/**
+ * Initialize logger from JavaScript
+ */
+export function initLogger(verbose: boolean): void;
+
+/**
+ * Initialize WASM module (call once from JS)
+ */
+export function start(): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly __wbg_wasmmodule_free: (a: number, b: number) => void;
+    readonly start: () => void;
+    readonly wasmmodule_dataCount: (a: number) => number;
+    readonly wasmmodule_elementCount: (a: number) => number;
+    readonly wasmmodule_encode: (a: number) => [number, number, number];
+    readonly wasmmodule_exportCount: (a: number) => number;
+    readonly wasmmodule_functionCount: (a: number) => number;
+    readonly wasmmodule_globalCount: (a: number) => number;
+    readonly wasmmodule_importCount: (a: number) => number;
+    readonly wasmmodule_memoryCount: (a: number) => number;
+    readonly wasmmodule_new: () => number;
+    readonly wasmmodule_parse: (a: any) => [number, number, number];
+    readonly wasmmodule_tableCount: (a: number) => number;
+    readonly wasmmodule_toJSON: (a: number) => [number, number, number, number];
+    readonly wasmmodule_typeCount: (a: number) => number;
+    readonly initLogger: (a: number) => void;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ crates/web/wasm_parser_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_wasmmodule_free: (a: number, b: number) => void;
+export const start: () => void;
+export const wasmmodule_dataCount: (a: number) => number;
+export const wasmmodule_elementCount: (a: number) => number;
+export const wasmmodule_encode: (a: number) => [number, number, number];
+export const wasmmodule_exportCount: (a: number) => number;
+export const wasmmodule_functionCount: (a: number) => number;
+export const wasmmodule_globalCount: (a: number) => number;
+export const wasmmodule_importCount: (a: number) => number;
+export const wasmmodule_memoryCount: (a: number) => number;
+export const wasmmodule_new: () => number;
+export const wasmmodule_parse: (a: any) => [number, number, number];
+export const wasmmodule_tableCount: (a: number) => number;
+export const wasmmodule_toJSON: (a: number) => [number, number, number, number];
+export const wasmmodule_typeCount: (a: number) => number;
+export const initLogger: (a: number) => void;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
